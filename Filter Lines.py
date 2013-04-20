6315162709dc15945ce0faed3cdfd068176800e6
David Peckham <dave.peckham@me.com>
2013-04-20 05:49:09 -0700

    Now simply imports commands from source files in the same folder on ST2 and ST3

--- Filter Lines.py
+++ Filter Lines.py
@@ -4,16 +4,5 @@ import sublime
 import sublime_plugin
 
 
-st_version = 2
-if sublime.version() == '' or int(sublime.version()) > 3000:
-    st_version = 3
-
-try:
-    # Python 3
-    from .commands import *
-
-except (ValueError):
-    # Python 2
-    from filter_to_lines_command import FilterToLinesCommand
-    from fold_to_lines_command import FoldToLinesCommand
-    # from commands import *
+from filter_to_lines_command import FilterToLinesCommand
+from fold_to_lines_command import FoldToLinesCommand
